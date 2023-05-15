759b3a91469bd369781519a57de1ab756adb8285
Mattias Björk <mattias.bjork.id1@gmail.com>
2023-05-16 00:34:39 +0200

    testing push

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ MENU = {
         "ingredients": {
             "water": 50,
             "coffee": 18,
+            "milk": 0,
         },
         "cost": 1.5,
     },
@@ -21,15 +22,70 @@ MENU = {
             "coffee": 24,
         },
         "cost": 3.0,
-    }
+    },
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "water": {
+        "amount": 300,
+        "unit": "ml"
+    },
+    "milk": {
+        "amount": 200,
+        "unit": "ml",
+    },
+    "coffee": {
+        "amount": 100,
+        "unit": "g",
+    },
+    "money": {
+        "amount": 0,
+        "unit": "USD",
+    },
 }
 
-# adding a line
-# TODO maybe a todo also
-print("Snakes")
+app_status = "run"
+
+
+def coin_handler(menu_item):
+    total_cost = MENU[menu_item]["cost"]
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How many pennies?: "))
+    total_input = round(quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01, 2)
+    if total_input >= total_cost:
+        print(f"Here is ${(total_input - total_cost):.2f} in change.")
+        return True
+    else:
+        print("Insufficient funds, returning coins.")
+        return False
+
+
+def coffee_ui(input_command):
+    global app_status
+    if input_command == "end":
+        app_status = "end"
+    elif input_command == "latte" or input_command == "cappuccino" or input_command == "espresso":
+        if (MENU[input_command]["ingredients"]["water"] <= resources["water"]['amount']) and (
+                MENU[input_command]["ingredients"]["coffee"] <= resources["coffee"]['amount']) and (
+                MENU[input_command]["ingredients"]["milk"] <= resources["milk"]['amount']):
+            if coin_handler(input_command):
+                resources["water"]['amount'] -= MENU[input_command]["ingredients"]["water"]
+                resources["coffee"]['amount'] -= MENU[input_command]["ingredients"]["coffee"]
+                resources["milk"]['amount'] -= MENU[input_command]["ingredients"]["milk"]
+                resources["money"]['amount'] += MENU[input_command]["cost"]
+                print(f"Enjoy your {input_command}, enjoy!")
+    elif input_command == "report":
+        for item in resources:
+            print(f"{item}: {resources[item]['amount']}{resources[item]['unit']}")
+        app_status = "restart"
+    else:
+        print("Please use a proper command!")
+
+
+while True:
+
+    coffee_ui(input("What would you like? (espresso/latte/cappuccino): "))
+    if app_status == "end":
+        break
